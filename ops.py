20370e0568f3e9e3f732c9c25a91789226820857
ChengChen-ai <73919670+ChengChen-ai@users.noreply.github.com>
2021-12-19 19:14:15 +0800

    Update ops.py

--- ops.py
+++ ops.py
@@ -32,26 +32,6 @@ def get_gradients_loss(I, R):
     return torch.mean \
         (gradients_I_x * torch.exp(-10 * avg(R_gray, 'x')) + gradients_I_y * torch.exp(-10 * avg(R_gray, 'y')))
 
-
-def edge_conv2d(im):
-    # 用nn.Conv2d定义卷积操作
-    conv_op = nn.Conv2d(3, 3, kernel_size=3, padding=1, bias=False)
-    # 定义sobel算子参数，所有值除以3个人觉得出来的图更好些
-    sobel_kernel = np.array([[-1, -1, -1], [-1, 8, -1], [-1, -1, -1]], dtype='float32') / 3
-    # 将sobel算子转换为适配卷积操作的卷积核
-    sobel_kernel = sobel_kernel.reshape((1, 1, 3, 3))
-    # 卷积输出通道，这里我设置为3
-    sobel_kernel = np.repeat(sobel_kernel, 3, axis=1)
-    # 输入图的通道，这里我设置为3
-    sobel_kernel = np.repeat(sobel_kernel, 3, axis=0)
-
-    conv_op.weight.data = torch.from_numpy(sobel_kernel).cuda(0)
-
-    edge_detect = conv_op(im)
-
-    # edge_detect = edge_detect.squeeze().detach().numpy()
-    return edge_detect
-
 class MeanShift(nn.Conv2d):
     def __init__(self, rgb_range, rgb_mean, rgb_std, sign=-1):
         super(MeanShift, self).__init__(3, 3, kernel_size=1)
@@ -63,34 +43,6 @@ class MeanShift(nn.Conv2d):
         # self.requires_grad = False
         self.weight.requires_grad = False
         self.bias.requires_grad = False
-
-
-def calc_psnr(img1, img2):
-    ### args:
-        # img1: [h, w, c], range [0, 255]
-        # img2: [h, w, c], range [0, 255]
-    diff = (img1 - img2) / 255.0
-    diff[:,:,0] = diff[:,:,0] * 65.738 / 256.0
-    diff[:,:,1] = diff[:,:,1] * 129.057 / 256.0
-    diff[:,:,2] = diff[:,:,2] * 25.064 / 256.0
-
-    diff = np.sum(diff, axis=2)
-    mse = np.mean(np.power(diff, 2))
-    return -10 * math.log10(mse)
-
-
-def construct_ideal_affinity_matrix(label, label_size, num_classes):
-    scaled_labels = F.interpolate(
-        label.float(), label_size, mode="nearest")
-    scaled_labels = scaled_labels.squeeze_(1).long()
-    scaled_labels[scaled_labels == 255] = num_classes
-    one_hot_labels = F.one_hot(scaled_labels, num_classes + 1)
-    one_hot_labels = one_hot_labels.view(
-        one_hot_labels.size(0), -1, num_classes + 1).float()
-    ideal_affinity_matrix = torch.bmm(one_hot_labels,
-                                      one_hot_labels.permute(0, 2, 1))
-    return ideal_affinity_matrix
-
 
 class ConvBlock(torch.nn.Module):
     def __init__(self, input_size, output_size, kernel_size=3, stride=2, padding=(1,1), activation='relu', batch_norm=True):
@@ -168,52 +120,3 @@ class ResnetBlock(torch.nn.Module):
     def forward(self, x):
         out = self.resnet_block(x)
         return out
-
-
-def conv1x1(in_channels, out_channels, stride=1):
-    return nn.Conv2d(in_channels, out_channels, kernel_size=1,
-                     stride=stride, padding=0, bias=True)
-
-def conv3x3(in_channels, out_channels, stride=1):
-    return nn.Conv2d(in_channels, out_channels, kernel_size=3,
-                     stride=stride, padding=1, bias=True)
-
-
-class ResBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, stride=1, downsample=None, res_scale=1):
-        super(ResBlock, self).__init__()
-        self.res_scale = res_scale
-        self.conv1 = conv3x3(in_channels, out_channels, stride)
-        self.relu = nn.ReLU(inplace=True)
-        self.conv2 = conv3x3(out_channels, out_channels)
-
-    def forward(self, x):
-        x1 = x
-        out = self.conv1(x)
-        out = self.relu(out)
-        out = self.conv2(out)
-        out = out * self.res_scale + x1
-        return out
-
-class MergeTail(nn.Module):
-    def __init__(self, n_feats):
-        super(MergeTail, self).__init__()
-        self.conv13 = conv1x1(n_feats, n_feats)
-        self.conv23 = conv1x1(n_feats, n_feats)
-        self.conv_merge = conv3x3(n_feats * 3, n_feats)
-        self.conv_tail1 = conv3x3(n_feats, n_feats // 2)
-        self.conv_tail2 = conv1x1(n_feats // 2, 3)
-
-    def forward(self, x1, x2, x3):
-        x13 = F.interpolate(x1, scale_factor=4, mode='bicubic')
-        x13 = F.relu(self.conv13(x13))
-        x23 = F.interpolate(x2, scale_factor=2, mode='bicubic')
-        x23 = F.relu(self.conv23(x23))
-
-        x = F.relu(self.conv_merge(torch.cat((x3, x13, x23), dim=1)))
-        x = self.conv_tail1(x)
-        x = self.conv_tail2(x)
-        x = torch.clamp(x, -1, 1)
-
-        return x
-
